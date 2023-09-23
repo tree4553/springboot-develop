5abf1a167ac67ac6e32a216975cd40b109dd3a69
tree4553 <tree4553@naver.com>
2023-09-24 01:04:18 +0900

    Member Entity, Repository, Service, Controller 테스트 h2, lombok 의존성 추가 JPA를 통한 H2 데이터베이스 사용

--- src/main/java/me/jeongcheol/springbootdeveloper/TestController.java
+++ src/main/java/me/jeongcheol/springbootdeveloper/TestController.java
@@ -1,13 +1,25 @@
 package me.jeongcheol.springbootdeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController // 라우터 역할을 하는 애너테이션
 // 라우터란 HTTP 요청과 메서드를 연결하는 장치이다.
 public class TestController {
+
+    @Autowired
+    TestService testService;
+
     @GetMapping("/test")
-    public String test() {
-        return "Hello, world!";
+    public List<Member> getAllMembers() {
+        List<Member> members = testService.getAllMembers();
+        return members;
     }
+
+
+
+
 }
