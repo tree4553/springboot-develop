ec05287ff2d40865fb9dbc94578cc21d177e84ab
tree4553 <tree4553@naver.com>
2023-09-24 23:33:11 +0900

    Test Code 예제

--- src/test/java/me/jeongcheol/springbootdeveloper/TestControllerTest.java
+++ src/test/java/me/jeongcheol/springbootdeveloper/TestControllerTest.java
@@ -2,14 +2,22 @@ package me.jeongcheol.springbootdeveloper;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest // 테스트용 애플리케이션 컨텍스트 생성
 @AutoConfigureMockMvc   // MockMvc 생성 및 자동 구성
@@ -33,6 +41,24 @@ class TestControllerTest {
     @AfterEach  // 테스트 실행 후 실행하는 메서드
     public void cleanUp() {
         memberRepository.deleteAll();
+    }
+
+    @DisplayName("getAllMembers: 모든 멤버 조회 성공")
+    @Test
+    public void getAllMembers() throws Exception {
+        // given
+        final String url = "/test";
+        Member savedMember = memberRepository.save(new Member(1L, "이정철"));
+
+        // when
+        final ResultActions result = mockMvc.perform(get(url)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        result
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(savedMember.getId()))
+                .andExpect(jsonPath("$[0].name").value(savedMember.getName()));
     }
 
 }
